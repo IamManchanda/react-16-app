f467a392e394917a0d0dce08eaf84069bfa49470
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-03-31 19:57:56 +0530

    Shield DX Switching!

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import AppPerson from './person/AppPerson';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Roman Reigns',
+        age: 32,
+        finishingMove: 'SuperMan Punch',
+      },
+      {
+        name: 'Dean Ambrose',
+        age: 32,
+        finishingMove: 'Dirty Deeds',
+      },
+      {
+        name: 'Seth Rollins',
+        age: 31,
+        finishingMove: 'Curb Stomp',
+      },
+    ],
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {
+          name: 'Shawn Michaels',
+          age: 52,
+          finishingMove: 'Sweet Chin Music',
+        },
+        {
+          name: 'XPac',
+          age: 45,
+          finishingMove: 'X-Factor',
+        },
+        {
+          name: 'Triple H',
+          age: 48,
+          finishingMove: 'Pedigree',
+        },
+      ],
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Hello React 16!</h1>
+        <h1>This is S.H.I.E.L.D!!!</h1>
+        <p>But if you click the button below, it becomes <strong>DX</strong></p>
+        <button 
+          className="button radius bordered shadow primary"
+          onClick={this.switchNameHandler}>
+          Switch Wrestlers
+        </button>
+        <AppPerson name={this.state.persons[0].name} age={this.state.persons[0].age}>
+          {this.state.persons[0].finishingMove}
+        </AppPerson>
+        <AppPerson name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          {this.state.persons[1].finishingMove}
+        </AppPerson>
+        <AppPerson name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          {this.state.persons[2].finishingMove}
+        </AppPerson>
       </div>
     );
   }
--- src/components/person/AppPerson.js
+++ src/components/person/AppPerson.js
@@ -1,8 +1,16 @@
-const AppPerson = () => {
+import React from 'react';
+
+const AppPerson = (props) => {
+  console.log(props);
   return (
-    <p className="app-Person">
-      I am a Person
-    </p>
+    <div className="app-Person">
+      <p className="app-Person_NameAge">
+        I am { props.name } and I am { props.age } years old.
+      </p>
+      <p className="app-Person_FinishingMove">
+        My Finishing Move is: { props.children }
+      </p>
+    </div>
   );
 };
 
