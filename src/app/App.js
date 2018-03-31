60117c37d8660b9281af41283df71a4f4554be2d
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-03-31 18:15:57 +0530

    Sass configurations!

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <h1>Hello React 16!</h1>
       </div>
     );
