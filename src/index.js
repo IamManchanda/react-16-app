309a02842437b4a437f714ce43c18044712fce20
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-03-31 18:21:46 +0530

    Folder Reorganisation!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
-import App from './app/App';
+import './watch-css/style.css';
+import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
