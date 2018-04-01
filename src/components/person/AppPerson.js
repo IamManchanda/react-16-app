a5b18580503d75e67e86adb9acacb38a1f013084
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-04-01 21:44:26 +0530

    Use Modifier classes for scoped css purpose!

--- src/components/person/AppPerson.js
+++ src/components/person/AppPerson.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import scopedClasses from './__scopedClasses.css';
+import modifierClasses from './__modifierClasses.css';
 
 const AppPerson = (props) => {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+  if (randomNumber >= 5) {
+    this.randomModifierClass = modifierClasses.isCalloutSecondary;
+  } else {
+    this.randomModifierClass = modifierClasses.isCalloutLight;
+  }
+
   return (
-    <div className={ `app-Person callout ${scopedClasses.Person}` }>
+    <div className={ `app-Person callout ${this.randomModifierClass}` }>
       <button className="close-button" aria-label="Dismiss alert" type="button">
         <span aria-hidden="true"
           onClick={ props.destroy }>&times;</span>
       </button>
       <div className="app-Person_Content">
+        <p>Random Number: { randomNumber }</p>
         <p>I am { props.name } and I am { props.age } years old.</p>
         <p>My Finishing Move is: { props.children }</p>
         <input type="text" placeholder="Enter New Name"
